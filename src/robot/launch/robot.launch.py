6f16206517f4feb6c086eb9b80b1f2bbed8a45e1
Alexander-Nyagolov <nyagolovlakesnader@gmail.com>
2025-09-25 13:05:55 +0200

    Update RViz configuration in robot launch file to use nav2 default view

--- src/robot/launch/robot.launch.py
+++ src/robot/launch/robot.launch.py
@@ -94,7 +94,7 @@ def generate_launch_description():
         package='rviz2',
         executable='rviz2',
         name='rviz2',
-        arguments=['-d', os.path.join(ros_dir, 'rplidar_ros', 'rviz', 'rplidar.rviz')],
+        arguments=['-d', os.path.join(ros_dir, 'nav2_bringup', 'rviz', 'nav2_default_view.rviz')],
         output='screen'
     )
 
