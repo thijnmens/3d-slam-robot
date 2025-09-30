be3cb9fd1d76d5a5cdb04550e176f217d3b1633b
Thijnmens <thijnsmulders04@gmail.com>
2025-09-30 10:27:39 +0200

    Add robot and wheel dataclasses

--- src/robot/robot/motor_controller.py
+++ src/robot/robot/motor_controller.py
@@ -1,93 +1,111 @@
+from math import pi
+from time import time
+
+from gpiozero import Motor, PWMOutputDevice, RotaryEncoder
+from simple_pid import PID
+
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
-from gpiozero import Motor, PWMOutputDevice, RotaryEncoder
-from simple_pid import PID
-from time import time
-from math import pi
+from .dataclasses.robot import Robot
 
 
 class MotorController(Node):
-	def __init__(self, pulses_per_rev=396, R=0.03):
-		super().__init__('motor_controller')
-		self.pulses_per_rev = pulses_per_rev
-		self.R = R
-		self.wheel_sign = {"FL": 1, "FR": -1, "RL": 1, "RR": -1}
-		self.motors = {}
-		self.encoders = {}
-		self.pids = {}
-		self.setpoints = {"FL": 0.0, "FR": 0.0, "RL": 0.0, "RR": 0.0}
-		config = {
-			"FL": {"in1": 24 , "in2": 23,  "ena": 18, "enc_a": 25,  "enc_b": 27},
-			"FR": {"in1": 1, "in2": 22, "ena": 12, "enc_a": 16, "enc_b": 20},
-			"RL": {"in1": 21, "in2": 26, "ena": 19, "enc_a": 0,  "enc_b": 11},
-			"RR": {"in1": 6,  "in2": 5, "ena": 13, "enc_a": 9, "enc_b": 10},
-		}
-		self.prev_pulses = {"FL": 0, "FR": 0, "RL": 0, "RR": 0}
-		for wheel, pins in config.items():
-			motor = Motor(forward=pins["in1"], backward=pins["in2"], pwm=False)
-			ena = PWMOutputDevice(pins["ena"], frequency=1000, initial_value=0)
-			enc = RotaryEncoder(a=pins["enc_a"], b=pins["enc_b"], max_steps=0)
-			self.motors[wheel] = {"motor": motor, "ena": ena}
-			self.encoders[wheel] = enc
-			pid = PID(1.0, 0.1, 0.05, setpoint=0)
-			pid.output_limits = (0, 1)
-			self.pids[wheel] = pid
-		self.subscription = self.create_subscription(
-			Float32MultiArray, 'wheel_setpoints', self.setpoints_callback, 10
-		)
-		self.wheel_state_pub = self.create_publisher(Float32MultiArray, 'wheel_speeds', 10)
-		self.last_time = time()
-		self.create_timer(0.02, self.control_loop)
+    def __init__(self, pulses_per_rev=396, R=0.03):
+        super().__init__('motor_controller')
 
-	def setpoints_callback(self, msg: Float32MultiArray):
-		if len(msg.data) >= 4:
-			self.setpoints["FL"], self.setpoints["FR"], self.setpoints["RL"], self.setpoints["RR"] = msg.data[:4]
+        # Get robot data
+        self.robot = Robot()
 
-	def control_loop(self):
-		now = time()
-		dt = max(now - self.last_time, 1e-3)
-		self.last_time = now
-		speeds = []
-		for wheel in ["FL", "FR", "RL", "RR"]:
-			enc = self.encoders[wheel]
-			pulses = self.wheel_sign[wheel] * (enc.steps - self.prev_pulses[wheel])
-			self.prev_pulses[wheel] = enc.steps
-			revs = pulses / self.pulses_per_rev
-			distance = revs * 2 * pi * self.R
-			speed = distance / dt
-			self.pids[wheel].setpoint = abs(self.setpoints[wheel])
-			duty = self.pids[wheel](abs(speed))
-			motor = self.motors[wheel]["motor"]
-			ena = self.motors[wheel]["ena"]
-			if self.setpoints[wheel] >= 0.1:
-				ena.value = duty
-				motor.forward()
-			elif self.setpoints[wheel] <= -0.1:
-				ena.value = duty
-				motor.backward()
-			else:
-				ena.value = 0
-				motor.stop()
-			speeds.append(speed)
-		msg = Float32MultiArray()
-		msg.data = speeds
-		self.wheel_state_pub.publish(msg)
+        # Motor classes
+        self.motors = {}
+        self.encoders = {}
+        self.pids = {}
 
-	def stop_all(self):
-		for wheel in self.motors:
-			self.motors[wheel]["motor"].stop()
-			self.motors[wheel]["ena"].value = 0.0
+        # PID requirements
+        self.setpoints = {"front_left": 0.0, "front_right": 0.0, "rear_left": 0.0, "rear_right": 0.0}
+        self.prev_pulses = {"front_left": 0, "front_right": 0, "rear_left": 0, "rear_right": 0}
+
+        # Configure motor driving classes
+        for wheel in self.robot:
+            motor = Motor(forward=wheel.in1, backward=wheel.in2, pwm=False)
+            ena = PWMOutputDevice(wheel.pwm, frequency=1000, initial_value=0)
+            enc = RotaryEncoder(a=wheel.encoder_a, b=wheel.encoder_b, max_steps=0)
+
+            self.motors[wheel.name] = {"motor": motor, "ena": ena}
+            self.encoders[wheel.name] = enc
+
+            pid = PID(1.0, 0.1, 0.05, setpoint=0)
+            pid.output_limits = (0, 1)
+
+            self.pids[wheel.name] = pid
+
+        self.get_logger().info("Creating subscription on /wheel_setpoints topic")
+        self.wheel_setpoints_sub = self.create_subscription(
+            Float32MultiArray, '/wheel_setpoints', self.setpoints_callback, 10
+        )
+
+        self.get_logger().info("Creating publisher on /wheel_speeds topic")
+        self.wheel_speeds_pub = self.create_publisher(Float32MultiArray, '/wheel_speeds', 10)
+
+        self.last_time = time()
+
+        self.create_timer(0.02, self.control_loop)
+
+    def setpoints_callback(self, msg: Float32MultiArray):
+        if len(msg.data) >= 4:
+            self.setpoints["FL"], self.setpoints["FR"], self.setpoints["RL"], self.setpoints["RR"] = msg.data[:4]
+
+    def control_loop(self):
+        now = time()
+        dt = max(now - self.last_time, 1e-3)
+        self.last_time = now
+        speeds = []
+        for wheel in self.robot:
+
+            enc = self.encoders[wheel.name]
+            pulses = wheel.forward * (enc.steps - self.prev_pulses[wheel.name])
+            self.prev_pulses[wheel.name] = enc.steps
+
+            revs = pulses / self.pulses_per_rev
+            distance = revs * 2 * pi * (self.robot.wheel_diameter / 2)
+            speed = distance / dt
+
+            self.pids[wheel.name].setpoint = abs(self.setpoints[wheel.name])
+
+            duty = self.pids[wheel.name](abs(speed))
+            motor = self.motors[wheel.name]["motor"]
+            ena = self.motors[wheel.name]["ena"]
+
+            if self.setpoints[wheel.name] >= 0.1:
+                ena.value = duty
+                motor.forward()
+            elif self.setpoints[wheel.name] <= -0.1:
+                ena.value = duty
+                motor.backward()
+            else:
+                ena.value = 0
+                motor.stop()
+            speeds.append(speed)
+
+        msg = Float32MultiArray()
+        msg.data = speeds
+        self.wheel_speeds_pub.publish(msg)
+
+    def stop_all(self):
+        for wheel in self.motors:
+            self.motors[wheel]["motor"].stop()
+            self.motors[wheel]["ena"].value = 0.0
 
 
 def main(args=None):
-	rclpy.init(args=args)
-	node = MotorController()
-	try:
-		rclpy.spin(node)
-	except KeyboardInterrupt:
-		pass
-	finally:
-		node.stop_all()
-		node.destroy_node()
-		rclpy.shutdown()
+    rclpy.init(args=args)
+    node = MotorController()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.stop_all()
+        node.destroy_node()
+        rclpy.shutdown()
--- src/robot/robot/odometry_node.py
+++ src/robot/robot/odometry_node.py
@@ -1,99 +1,134 @@
+from math import sin, cos, pi
+
 import rclpy
-from rclpy.node import Node
-from std_msgs.msg import Float32MultiArray
-from nav_msgs.msg import Odometry
 from geometry_msgs.msg import TransformStamped
+from nav_msgs.msg import Odometry
+from rclpy.duration import Duration
+from rclpy.node import Node
+from rclpy.time import Time
+from .dataclasses.robot import Robot
+from std_msgs.msg import Float32MultiArray
 from tf2_ros import TransformBroadcaster
 from tf_transformations import quaternion_from_euler
-from math import sin, cos, pi
 
 
 class OdometryNode(Node):
-	def __init__(self, L=0.20, W=0.21):
-		super().__init__('odometry')
-		self.L = L
-		self.W = W
-		self.x = 0.0
-		self.y = 0.0
-		self.theta = 0.0
-		self.last_time = self.get_clock().now().nanoseconds * 1e-9
-		self.subscription = self.create_subscription(
-			Float32MultiArray, 'wheel_speeds', self.wheel_speeds_callback, 10
-		)
-		self.odom_pub = self.create_publisher(Odometry, 'odom', 10)
-		self.tf_broadcaster = TransformBroadcaster(self)
+    def __init__(self):
+        super().__init__('odometry')
 
-	def wheel_speeds_callback(self, msg: Float32MultiArray):
-		if len(msg.data) < 4:
-			return
-		now_time = self.get_clock().now()
-		now_ros = now_time.nanoseconds * 1e-9
-		dt = max(now_ros - self.last_time, 1e-3)
-		self.last_time = now_ros
-		
-		v_fl, v_fr, v_rl, v_rr = msg.data[:4]
-		vx = ((v_fl + v_fr + v_rl + v_rr) / 4.0)
-		vy = ((-v_fl + v_fr + v_rl - v_rr) / 4.0)
-		wz = (-v_fl + v_fr - v_rl + v_rr) / (4.0 * (self.L + self.W))
+        # Robot data
+        self.robot = Robot()
 
-		vx = vx * -1.26
-		vy = vy * 1.26
-		wz = wz * -2.1
+        # Robot location
+        self.x = 0.0
+        self.y = 0.0
+        self.yaw = 0.0
 
-		self.x += vx * cos(self.theta) * dt - vy * sin(self.theta) * dt
-		self.y += vx * sin(self.theta) * dt + vy * cos(self.theta) * dt
-		self.theta += wz * dt
+        # Delta time
+        self.last_time = self.get_clock().now().nanoseconds * 1e-9
 
-		while self.theta > pi:
-			self.theta -= 2 * pi
-		while self.theta < -pi:
-			self.theta += 2 * pi
-		
-		odom = Odometry()
+        # Wheel speed subscriber
+        self.get_logger().info("Creating subscription on /wheel_speeds topic")
+        self.subscription = self.create_subscription(
+            Float32MultiArray, '/wheel_speeds', self.wheel_speeds_callback, 10
+        )
 
-		odom.header.stamp = now_time.to_msg()
-		odom.header.frame_id = "odom"
-		odom.child_frame_id = "base_link"
+        # Odometry publisher
+        self.get_logger().info("Creating publisher on /odom topic")
+        self.odom_pub = self.create_publisher(Odometry, 'odom', 10)
 
-		odom.pose.pose.position.x = self.x
-		odom.pose.pose.position.y = self.y
+        # TF tree broadcaster
+        self.get_logger().info("Creating TF broadcaster")
+        self.tf_broadcaster = TransformBroadcaster(self)
 
-		q = quaternion_from_euler(0.0, 0.0, self.theta)
-		odom.pose.pose.orientation.x = q[0]
-		odom.pose.pose.orientation.y = q[1]
-		odom.pose.pose.orientation.z = q[2]
-		odom.pose.pose.orientation.w = q[3]
+    def wheel_speeds_callback(self, msg: Float32MultiArray):
+        # Check if all wheels are included
+        if len(msg.data) < 4:
+            return
 
-		odom.twist.twist.linear.x = vx
-		odom.twist.twist.linear.y = vy
-		odom.twist.twist.angular.z = wz
+        # Save time for delta time calculations
+        now_time = self.get_clock().now()
+        now_ros = now_time.nanoseconds * 1e-9
+        dt = max(now_ros - self.last_time, 1e-3)
+        self.last_time = now_ros
 
-		self.odom_pub.publish(odom)
+        # Calculate velocities
+        vel_fl, vel_fr, vel_rl, vel_rr = msg.data[:4]
+        vel_x = ((vel_fl + vel_fr + vel_rl + vel_rr) / 4.0)
+        vel_y = ((-vel_fl + vel_fr + vel_rl - vel_rr) / 4.0)
+        vel_yaw = (-vel_fl + vel_fr - vel_rl + vel_rr) / (4.0 * (self.L + self.W))
 
-		t = TransformStamped()
+        # Magic numbers
+        vel_x = vel_x * -1.26
+        vel_y = vel_y * 1.26
+        vel_yaw = vel_yaw * -2.1
 
-		t.header.stamp = now_time.to_msg()
-		t.header.frame_id = 'odom'
-		t.child_frame_id = 'base_link'
+        # Calculate new position
+        self.x += vel_x * cos(self.yaw) * dt - vel_y * sin(self.yaw) * dt
+        self.y += vel_x * sin(self.yaw) * dt + vel_y * cos(self.yaw) * dt
+        self.yaw += vel_yaw * dt
 
-		t.transform.translation.x = self.x
-		t.transform.translation.y = self.y
-		t.transform.translation.z = 0.0
+        # Normalize radials
+        while self.yaw > pi:
+            self.yaw -= 2 * pi
+        while self.yaw < -pi:
+            self.yaw += 2 * pi
 
-		t.transform.rotation.x = q[0]
-		t.transform.rotation.y = q[1]
-		t.transform.rotation.z = q[2]
-		t.transform.rotation.w = q[3]
+        # Publish new position to odometry
+        self.publish_odom(now_time.to_msg(), vel_x, vel_y, vel_yaw)
 
-		self.tf_broadcaster.sendTransform(t)
+        # Update transform tree
+        self.publish_tf(now_time.to_msg())
+
+    def publish_odom(self, stamp: Time, vel_x: float, vel_y: float, vel_yaw: float):
+        odom = Odometry()
+
+        odom.header.stamp = stamp
+        odom.header.frame_id = "odom"
+        odom.child_frame_id = "base_link"
+
+        odom.pose.pose.position.x = self.x
+        odom.pose.pose.position.y = self.y
+
+        q = quaternion_from_euler(0.0, 0.0, self.yaw)
+        odom.pose.pose.orientation.x = q[0]
+        odom.pose.pose.orientation.y = q[1]
+        odom.pose.pose.orientation.z = q[2]
+        odom.pose.pose.orientation.w = q[3]
+
+        odom.twist.twist.linear.x = vel_x
+        odom.twist.twist.linear.y = vel_y
+        odom.twist.twist.angular.z = vel_yaw
+
+        self.odom_pub.publish(odom)
+
+    def publish_tf(self, stamp: Time):
+        t = TransformStamped()
+
+        t.header.stamp = stamp
+        t.header.frame_id = 'odom'
+        t.child_frame_id = 'base_link'
+
+        t.transform.translation.x = self.x
+        t.transform.translation.y = self.y
+        t.transform.translation.z = 0.0
+
+        q = quaternion_from_euler(0.0, 0.0, self.yaw)
+        t.transform.rotation.x = q[0]
+        t.transform.rotation.y = q[1]
+        t.transform.rotation.z = q[2]
+        t.transform.rotation.w = q[3]
+
+        self.tf_broadcaster.sendTransform(t)
+
 
 def main(args=None):
-	rclpy.init(args=args)
-	node = OdometryNode()
-	try:
-		rclpy.spin(node)
-	except KeyboardInterrupt:
-		pass
-	finally:
-		node.destroy_node()
-		rclpy.shutdown()
+    rclpy.init(args=args)
+    node = OdometryNode()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
