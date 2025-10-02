ea7477e96988942c1767afabc8e06141fa382fc2
Thijnmens <thijnsmulders04@gmail.com>
2025-10-02 10:38:50 +0200

    Navigation launch file

--- src/robot/launch/navigation.launch.py
+++ src/robot/launch/navigation.launch.py
@@ -12,28 +12,7 @@ def generate_launch_description():
 
     # ROS Directory
     ros_dir = '/opt/ros/jazzy/share/'
-
-    # Joystick Reader
-    joy = Node(
-        package='joy',
-        executable='joy_node',
-        name='joy_node',
-        output='screen',
-        parameters=[{
-            # 'dev': '/dev/input/js0',
-        }]
-    )
-
-    # Joystick to Twist converter
-    joy2twist_share = get_package_share_directory('ros2_joy_twist')
-    mappings = os.path.join(joy2twist_share, 'mappings.yaml')
-    joy2twist = Node(
-        package='ros2_joy_twist',
-        executable='joy_to_twist',
-        name='joy_to_twist',
-        output='screen',
-        parameters=[mappings],
-    )
+    package_dir = get_package_share_directory('robot')
 
     # Motor driver
     motor_driver = Node(
@@ -71,40 +50,14 @@ def generate_launch_description():
     # nav2
     nav2 = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(ros_dir, 'nav2_bringup', 'launch', 'navigation_launch.py')
+            os.path.join(ros_dir, 'nav2_bringup', 'launch', 'bringup_launch.py')
         ),
-        launch_arguments={'use_sim_time': 'False'}.items()
+        launch_arguments={'use_sim_time': 'False', 'use_localization': 'True', 'map': f'{package_dir}/map/map.yaml'}.items()
     )
-
-    # Slam_toolbox with explicit params
-    slam_toolbox_share = get_package_share_directory('robot')
-    slam_params_file = os.path.join(slam_toolbox_share, 'config', 'slam_toolbox.yaml')
-    slam_toolbox = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(ros_dir, 'slam_toolbox', 'launch', 'online_async_launch.py')
-        ),
-        launch_arguments={
-            'use_sim_time': 'False',
-            'slam_params_file': slam_params_file
-        }.items()
-    )
-
-    # Visualization
-    # rviz2 = Node(
-    #     package='rviz2',
-    #     executable='rviz2',
-    #     name='rviz2',
-    #     arguments=['-d', os.path.join(ros_dir, 'nav2_bringup', 'rviz', 'nav2_default_view.rviz')],
-    #     output='screen'
-    # )
 
     return LaunchDescription([
-        joy,
-        joy2twist,
         motor_driver,
         rplidar,
         static_tf_base_to_laser,
         nav2,
-        slam_toolbox,
-        # rviz2,
     ])
