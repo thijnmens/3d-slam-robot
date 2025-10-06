53c1e6b508c7323e51ee396d37d7af16227c5e91
kaiser-simo <smomkov@gmail.com>
2025-10-06 13:09:44 +0200

    fix

--- src/robot/robot/odometry_node.py
+++ src/robot/robot/odometry_node.py
@@ -24,11 +24,11 @@ class OdometryNode(Node):
 
         self.get_logger().info('Creating subscriber on /wheel_speeds topic')
         self.subscription = self.create_subscription(
-            Float32MultiArray, 'wheel_speeds', self.wheel_speeds_callback
+            Float32MultiArray, 'wheel_speeds', self.wheel_speeds_callback, 10
         )
 
         self.get_logger().info('Creating publisher on /odom topic')
-        self.odom_pub = self.create_publisher(Odometry, 'odom')
+        self.odom_pub = self.create_publisher(Odometry, 'odom', 10)
 
         self.get_logger().info('Creating TF tree broadcaster')
         self.tf_broadcaster = TransformBroadcaster(self)
@@ -57,7 +57,7 @@ class OdometryNode(Node):
         vel_x = ((vel_front_left + vel_front_right + vel_rear_left + vel_rear_right) / 4.0)
         vel_y = ((-vel_front_left + vel_front_right + vel_rear_left - vel_rear_right) / 4.0)
         vel_yaw = (-vel_front_left + vel_front_right - vel_rear_left + vel_rear_right) / (
-                    4.0 * (self.robot.robot_length + self.robot.robot_width) / 10000)
+                    4.0 * (self.robot.robot_length + self.robot.robot_width) / 10000) ##
 
         # Magic numbers
         vel_x = vel_x * -1.26
