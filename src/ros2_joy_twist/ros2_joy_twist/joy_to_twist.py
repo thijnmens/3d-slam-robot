17641e26a6adc3a7b9e241c8f4cee20ea98d0555
Jordi <jordivandelagemaat@hotmail.nl>
2025-10-08 14:32:45 +0200

    brough back 67 stop random branching

--- src/ros2_joy_twist/ros2_joy_twist/joy_to_twist.py
+++ src/ros2_joy_twist/ros2_joy_twist/joy_to_twist.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import rclpy
+from geometry_msgs.msg import Twist
 from rclpy.node import Node
 from sensor_msgs.msg import Joy
-from geometry_msgs.msg import Twist
+
 
 class JoyToTwist(Node):
     def __init__(self):
@@ -12,6 +13,8 @@ class JoyToTwist(Node):
         self.declare_parameters('', [
             ('axis_linear.x', 1),
             ('axis_linear.y', 0),
+            ('axis_linear_dpad.x', -1),
+            ('axis_linear_dpad.y', -1),
             ('axis_angular.yaw', 3),
             ('scale_linear.x', 1.0),
             ('scale_linear.y', 1.0),
@@ -26,15 +29,17 @@ class JoyToTwist(Node):
         gp = self.get_parameter
         self.ax_lin_x = gp('axis_linear.x').value
         self.ax_lin_y = gp('axis_linear.y').value
-        self.ax_yaw   = gp('axis_angular.yaw').value
+        self.ax_lin_dpad_x = gp('axis_linear_dpad.x').value
+        self.ax_lin_dpad_y = gp('axis_linear_dpad.y').value
+        self.ax_yaw = gp('axis_angular.yaw').value
 
-        self.s_lin_x  = gp('scale_linear.x').value
-        self.s_lin_y  = gp('scale_linear.y').value
-        self.s_yaw    = gp('scale_angular.yaw').value
+        self.s_lin_x = gp('scale_linear.x').value
+        self.s_lin_y = gp('scale_linear.y').value
+        self.s_yaw = gp('scale_angular.yaw').value
 
-        self.enable_btn       = gp('enable_button').value
+        self.enable_btn = gp('enable_button').value
         self.enable_turbo_btn = gp('enable_turbo_button').value
-        self.turbo_scale      = gp('turbo_scale').value
+        self.turbo_scale = gp('turbo_scale').value
 
         self.deadzone = gp('deadzone').value
 
@@ -68,15 +73,21 @@ class JoyToTwist(Node):
         # Read axes with deadzone
         lin_x = self._dz(self._axis(msg.axes, self.ax_lin_x))
         lin_y = self._dz(self._axis(msg.axes, self.ax_lin_y))
-        yaw   = self._dz(self._axis(msg.axes, self.ax_yaw))
+        # Add D-pad axes to joystick axes
+        if self.ax_lin_dpad_x >= 0:
+            lin_x += self._dz(self._axis(msg.axes, self.ax_lin_dpad_x))
+        if self.ax_lin_dpad_y >= 0:
+            lin_y += self._dz(self._axis(msg.axes, self.ax_lin_dpad_y))
+        yaw = self._dz(self._axis(msg.axes, self.ax_yaw))
 
         # Build Twist (x forward, y strafe, z yaw)
         cmd = Twist()
-        cmd.linear.x  = lin_x * self.s_lin_x * turbo
-        cmd.linear.y  = lin_y * self.s_lin_y * turbo
-        cmd.angular.z = yaw   * self.s_yaw   * turbo
+        cmd.linear.x = lin_x * self.s_lin_x * turbo
+        cmd.linear.y = lin_y * self.s_lin_y * turbo
+        cmd.angular.z = yaw * self.s_yaw * turbo
 
         self.pub.publish(cmd)
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -88,6 +99,7 @@ def main(args=None):
     finally:
         node.destroy_node()
         rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
